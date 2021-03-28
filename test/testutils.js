6189e477802793255571155f1813913d21b05781
amk-dev <akash.k.mohan98@gmail.com>
2021-03-29 04:26:24 +0530

    add buildUser, buildContest factories

--- test/testutils.js
+++ test/testutils.js
@@ -1,3 +1,8 @@
+import mongoose from 'mongoose'
+import faker from 'faker'
+import User from '../src/models/User'
+import Contest from '../src/models/Contest'
+
 export function buildReq(overrides) {
 	return {
 		uid: 1001,
@@ -16,4 +21,41 @@ export function buildRes(overrides) {
 
 export function buildNext(impl) {
 	return jest.fn(impl).mockName('next')
+}
+
+const getContestId = () => mongoose.Types.ObjectId()
+const getUid = () => faker.random.uuid()
+const getEmail = () => faker.internet.email()
+const getPicture = () => faker.image.people()
+const getDisplayName = () => faker.name.firstName()
+const getYoutubeVideoId = () => faker.random.word()
+const getStartTime = () => faker.time.recent()
+
+export async function buildUser(overrides) {
+	let user = new User({
+		uid: getUid(),
+		email: getEmail(),
+		picture: getPicture(),
+		displayName: getDisplayName(),
+		...overrides,
+	})
+
+	await user.save()
+	return user
+}
+
+export async function buildContest(overrides) {
+	let contest = new Contest({
+		_id: getContestId(),
+		host_uid: getUid(),
+		host_picture: getPicture(),
+		host_display_name: getDisplayName(),
+		youtubeVideoId: getYoutubeVideoId(),
+		startTime: getStartTime(),
+		status: 'upcoming',
+		...overrides,
+	})
+
+	await contest.save()
+	return contest
 }
